9588055bafcd4b149303b931fe7335b81b3b6d4b
Shrihan-codrr <97533437+Shrihan-codrr@users.noreply.github.com>
2024-05-22 19:54:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,51 +1,47 @@
-from flask import Flask, request, jsonify, render_template,redirect,url_for, send_file
+import os
+
+from flask import Flask, request, jsonify, render_template, redirect, url_for,send_file
 from faker import Faker
 from twilio.jwt.access_token import AccessToken
 from twilio.jwt.access_token.grants import SyncGrant
 from werkzeug.utils import secure_filename
 
-
-
 app = Flask(__name__)
 fake = Faker()
-
-
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
-
-
+# Add your Twilio credentials
 @app.route('/token')
 def generate_token():
-    #add your twilio credentials
     TWILIO_ACCOUNT_SID = 'AC010d8bee6e0264fb47dcba199ca69da1'
     TWILIO_SYNC_SERVICE_SID = 'IS809c54078b86d1f48a82df52e678e265'
     TWILIO_API_KEY = 'SKbf3433f35b8fec0a16f8bb83715d97d9'
     TWILIO_API_SECRET = 'Q37dD66HVuBB0pTEz6qVfhgoXL6z6ogI'
 
-
     username = request.args.get('username', fake.user_name())
+
+    # create access token with credentials
     token = AccessToken(TWILIO_ACCOUNT_SID, TWILIO_API_KEY, TWILIO_API_SECRET, identity=username)
+    # create a Sync grant and add to token
     sync_grant_access = SyncGrant(TWILIO_SYNC_SERVICE_SID)
     token.add_grant(sync_grant_access)
     return jsonify(identity=username, token=token.to_jwt().decode())
 
-@app.route("/", methods = ["POST"])
+# A function to download text and store it in text file
+@app.route('/', methods=['POST'])
 def download_text():
     text_from_notepad = request.form['text']
-    with open('workfile.txt' , 'w') as f:
+    with open('workfile.txt', 'w') as f:
         f.write(text_from_notepad)
 
-    path_to_store_txt = 'workfile.txt'
+    path_to_store_txt = "workfile.txt"
 
-    return send_file(path_to_store_txt , as_attachment = True)
-    
+    return send_file(path_to_store_txt, as_attachment=True)
+
+
 if __name__ == "__main__":
-    app.run(port=5001)
-
-
-
+    app.run(host='localhost', port='5001', debug=True)
